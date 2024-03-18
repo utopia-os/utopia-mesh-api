904ed3d9bc662d2ec0966fa51881d36c23b675e3
David Rohmer <rohmer.david@gmail.com>
2024-03-18 15:23:00 +0100

    createDirectus

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # utopia-mesh-api
+
+
+## install
+
+
+```bash
+npm install
+```
+
+
+
+## development
+
+```bash
+npm run dev
+```
+
--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import {createDirectus, rest, graphql, readItems} from '@directus/sdk';
 
 const app = express();
 const port = parseInt(process.env.PORT || '3000'); // You can use any port that is free on your system
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+const client = createDirectus<any>('https://api.utopia-lab.org/').with(rest());
+
+app.get('/', async (req, res) => {
+    const result = await client.request(readItems('places'));
+    res.send(result);
 });
 
 app.listen(port, () => {
